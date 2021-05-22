9dee4aa96239382cef68da075436b10db60a3667
powerty <qwercy142@gmail.com>
2021-05-22 22:17:19 +0200

    Initial App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Board from "./components/Board/Board";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ textAlign: "center" }}> Candy Crush Clone </h1>
+      <Board />
     </div>
   );
-}
+};
 
 export default App;
