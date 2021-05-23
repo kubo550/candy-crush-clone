859cdb10cb7f9924dbae791df230e917546d39d5
powerty <qwercy142@gmail.com>
2021-05-23 23:51:31 +0200

    Move functions to utils

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,91 +1,25 @@
 import { useEffect, useState } from "react";
-
-import * as _ from "lodash";
+import {
+  addNewRow,
+  checkForAllMatches,
+  checkForMatchesHorizontal,
+  createBoard,
+  getValuesDown,
+  hasEmptyTiles,
+  matrixArray,
+} from "./Board.utils";
+import _ from "lodash";
 import * as S from "./Board.style";
 // import Candy from "../Candy/Candy";
 
 const BOARD_SIZE = 10;
-const COLORS = ["green", "orange", "red", "blue", "purple", "yellow"];
+// const COLORS = ["green", "orange", "red", "blue", "purple", "yellow"];
 
 const NUMBERS = [1, 2, 3];
 
 type Tile = {
   primaryColor: string;
   idx: number;
-};
-
-const matrixArray = (board: number[][]) =>
-  board[0].map((_, colIdx) => board.map(row => row[colIdx]));
-
-const createBoard = (size: number, numbers: number[]): number[][] => {
-  return Array(size)
-    .fill(null)
-    .map(() =>
-      Array(size)
-        .fill(0)
-        .map(() => _.sample(numbers)!)
-    );
-};
-
-const checkForMatchesHorizontal = (board: number[][]): number[][] => {
-  const tiles = _.cloneDeep(board);
-  const boardSize = tiles.length;
-
-  const newBoard: number[][] = [];
-
-  for (let i = 0; i < boardSize; i++) {
-    const row = tiles[i];
-    let queue: number[] = [row[0]];
-
-    // TODO error
-
-    for (let j = 1; j < boardSize + 1; j++) {
-      if (row[j] === queue[0]) {
-        queue.push(row[j]);
-      } else {
-        // todo the same code
-        if (queue.length >= 3) {
-          for (let index = 1; index <= queue.length; index++) {
-            row[j - index] = 0;
-          }
-        }
-
-        queue = [row[j]];
-      }
-    }
-    newBoard.push(row);
-  }
-  return newBoard;
-};
-
-const hasEmptyTiles = (board: number[][]): boolean =>
-  board.some(row => row.some(tile => tile === 0));
-
-const getValuesDown = (board: number[][]): number[][] => {
-  const newBoard = _.cloneDeep(board);
-
-  for (let i = 1; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-      if (newBoard[i][j] === 0) {
-        newBoard[i][j] = newBoard[i - 1][j];
-        newBoard[i - 1][j] = 0;
-      }
-    }
-  }
-  return newBoard;
-};
-
-const addNewRow = (board: number[][], numbers: number[]): number[][] => {
-  const newBoard = _.cloneDeep(board);
-  newBoard[0] = newBoard[0].map(num => (num === 0 ? _.sample(numbers)! : num));
-  return newBoard;
-};
-
-const checkForAllMatches = (board: number[][]) => {
-  let newBoard = _.cloneDeep(board);
-  newBoard = checkForMatchesHorizontal(newBoard);
-  newBoard = matrixArray(checkForMatchesHorizontal(matrixArray(newBoard)));
-  return newBoard;
 };
 
 const Board = () => {
