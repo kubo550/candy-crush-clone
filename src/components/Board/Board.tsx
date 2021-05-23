c0438b028599ffa987a5af0d47904e814f0d6cbc
powerty <qwercy142@gmail.com>
2021-05-24 00:14:47 +0200

    Make Gravity works

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -2,11 +2,9 @@ import { useEffect, useState } from "react";
 import {
   addNewRow,
   checkForAllMatches,
-  checkForMatchesHorizontal,
   createBoard,
   getValuesDown,
   hasEmptyTiles,
-  matrixArray,
 } from "./Board.utils";
 import _ from "lodash";
 import * as S from "./Board.style";
@@ -15,7 +13,14 @@ import * as S from "./Board.style";
 const BOARD_SIZE = 10;
 // const COLORS = ["green", "orange", "red", "blue", "purple", "yellow"];
 
-const NUMBERS = [1, 2, 3];
+const NUMBERS = [1, 2, 3, 4];
+
+const colorTransform = new Map([
+  [1, "red"],
+  [2, "blue"],
+  [3, "green"],
+  [4, "yellow"],
+]);
 
 type Tile = {
   primaryColor: string;
@@ -24,14 +29,28 @@ type Tile = {
 
 const Board = () => {
   const [board, setBoard] = useState(createBoard(BOARD_SIZE, NUMBERS));
+  const [prevBoard, setprevBoard] = useState(board);
   const [selectedTie, setSelectedTie] = useState<HTMLDivElement | null>(null);
   const [isUpdatingBoard, setIsUpdatingBoard] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
-      setBoard(prev => checkForAllMatches(prev));
+      if (hasEmptyTiles(board)) {
+        setBoard(prevBoard => addNewRow(getValuesDown(prevBoard), NUMBERS));
+        return;
+      }
+      if (!isUpdatingBoard && !_.isEqual(board, checkForAllMatches(board))) {
+        setBoard(prev => checkForAllMatches(prev));
+      } else {
+        setBoard(getValuesDown);
+        setIsUpdatingBoard(true);
+      }
+
+      if (_.isEqual(board, getValuesDown(board))) {
+        setIsUpdatingBoard(false);
+      }
     }, 1000);
-  }, []);
+  }, [board]);
 
   useEffect(() => {
     if (selectedTie?.id) {
@@ -75,7 +94,7 @@ const Board = () => {
               key={`${x}${y}`}
               onClick={handleClick}
               id={`${x}${y}`}
-              // style={{ backgroundColor: tile.primaryColor }}
+              style={{ backgroundColor: colorTransform.get(tile)! }}
             >
               {/* <Candy primaryColor={tile.primaryColor} secondaryColor='#74dce2' /> */}
               {tile}
