af50a368c6312b9b4606f7b711a0adf13ea8cd32
powerty <qwercy142@gmail.com>
2021-05-26 16:05:01 +0200

    Start use Candy Comp

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -6,28 +6,19 @@ import {
   addNewRow,
   getValuesDown,
   swipeTiles,
+  calculateDirection,
+  calculateNewPosition,
+  isValidMove,
 } from "./Board.utils";
+import candyColors from "../../data/candyColors";
 import _ from "lodash";
 import * as S from "./Board.style";
-// import Candy from "../Candy/Candy";
+import { TilePos } from "./Board.types";
+import Candy from "../Candy/Candy";
 
 const BOARD_SIZE = 10;
 
-const NUMBERS = [1, 2, 3, 4, 5, 6];
-
-const colorTransform = new Map([
-  [1, "red"],
-  [2, "blue"],
-  [3, "green"],
-  [4, "yellow"],
-  [5, "purple"],
-  [6, "orange"],
-]);
-
-type TilePos = {
-  x: number;
-  y: number;
-};
+const NUMBERS = [1, 2, 3, 4, 5];
 
 const Board = () => {
   const [board, setBoard] = useState(createBoard(BOARD_SIZE, NUMBERS));
@@ -35,12 +26,12 @@ const Board = () => {
   const [firstDraggedDiv, setFirstDraggedDiv] = useState<TilePos | null>(null);
 
   useEffect(() => {
-    setTimeout(() => {
+    const timeoutId = setTimeout(() => {
       if (hasEmptyTiles(board)) {
         setBoard(prevBoard => addNewRow(getValuesDown(prevBoard), NUMBERS));
         setIsPLayable(true);
 
-        return;
+        return () => clearInterval(timeoutId);
       }
       if (!isPlayable && !_.isEqual(board, checkForAllMatches(board))) {
         setBoard(prev => checkForAllMatches(prev));
@@ -55,6 +46,7 @@ const Board = () => {
     ) {
       setIsPLayable(false);
     }
+    return () => clearInterval(timeoutId);
   }, [board]);
 
   const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
@@ -81,44 +73,6 @@ const Board = () => {
       return;
     }
 
-    e.dataTransfer.effectAllowed = "copy";
-
-    const calculateDirection = (
-      offsetX: number,
-      offsetY: number
-    ): {
-      x: -1 | 0 | 1;
-      y: -1 | 0 | 1;
-    } => {
-      const isHorizontal = Math.abs(offsetX) > Math.abs(offsetY);
-
-      const x = offsetX > 0 ? 1 : -1;
-      const y = offsetY > 0 ? 1 : -1;
-
-      return {
-        x: 0 + (isHorizontal ? x : 0),
-        y: 0 + (!isHorizontal ? y : 0),
-      } as {
-        x: -1 | 0 | 1;
-        y: -1 | 0 | 1;
-      };
-    };
-
-    const calculateNewPosition = (
-      tilePos: TilePos,
-      direction: TilePos
-    ): TilePos => {
-      const x = tilePos.x + direction.x;
-      const y = tilePos.y + direction.y;
-
-      return { x, y };
-    };
-
-    const isValidMove = (newPos: TilePos, boardSize: number): boolean => {
-      const coords = Object.values(newPos);
-      return coords.every(coord => coord >= 0 && coord <= boardSize - 1);
-    };
-
     const offsetX = e.screenX - firstDraggedDiv.x;
     const offsetY = e.screenY - firstDraggedDiv.y;
 
@@ -139,20 +93,26 @@ const Board = () => {
       {isPlayable ? "Make a move" : "Combo"}
       <S.Wrapper>
         {board.map((row, y) =>
-          row.map((tile, x) => (
-            <S.Cell
-              key={`${x}${y}`}
-              id={`${x}${y}`}
-              style={{ backgroundColor: colorTransform.get(tile)! }}
-              draggable
-              onDragStart={handleDragStart}
-              onDragOver={handleDragOver}
-              onDragEnd={e => handleDragEnd(e, { y, x })}
-            >
-              {/* <Candy primaryColor={tile.primaryColor} secondaryColor='#74dce2' /> */}
-              {tile}
-            </S.Cell>
-          ))
+          row.map((tile, x) => {
+            const { primaryColor, secondaryColor } = candyColors[tile];
+
+            return (
+              <S.Cell
+                key={`${x}${y}`}
+                id={`${x}${y}`}
+                onDragStart={handleDragStart}
+                onDragOver={handleDragOver}
+                onDragEnd={e => handleDragEnd(e, { y, x })}
+                draggable
+              >
+                {tile ? (
+                  <Candy primaryColor={primaryColor} secondaryColor='#74dce2' />
+                ) : (
+                  ""
+                )}
+              </S.Cell>
+            );
+          })
         )}
       </S.Wrapper>
     </div>
