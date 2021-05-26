b206d5aad20a3ddf18c6a115ec2ac713182e95b9
powerty <qwercy142@gmail.com>
2021-05-26 16:05:43 +0200

    add CalculateDirection fn

--- src/components/Board/Board.utils.ts
+++ src/components/Board/Board.utils.ts
@@ -1,6 +1,7 @@
 import _ from "lodash";
+import { NextIndexes, TilePos } from "./Board.types";
 
-const matrixArray = (board: number[][]) =>
+const matrixArray = (board: number[][]): number[][] =>
     board[0].map((_, colIdx) => board.map(row => row[colIdx]));
 
 export const createBoard = (size: number, numbers: number[]): number[][] => {
@@ -23,13 +24,11 @@ export const checkForMatchesHorizontal = (board: number[][]): number[][] => {
         const row = tiles[i];
         let queue: number[] = [row[0]];
 
-        // TODO error
 
         for (let j = 1; j < boardSize + 1; j++) {
             if (row[j] === queue[0]) {
                 queue.push(row[j]);
             } else {
-                // todo the same code
                 if (queue.length >= 3) {
                     for (let index = 1; index <= queue.length; index++) {
                         row[j - index] = 0;
@@ -67,7 +66,7 @@ export const addNewRow = (board: number[][], numbers: number[]): number[][] => {
     return newBoard;
 };
 
-export const checkForAllMatches = (board: number[][]) => {
+export const checkForAllMatches = (board: number[][]): number[][] => {
     let newBoard = _.cloneDeep(board);
     newBoard = checkForMatchesHorizontal(newBoard);
     newBoard = matrixArray(checkForMatchesHorizontal(matrixArray(newBoard)));
@@ -78,9 +77,9 @@ export const checkForAllMatches = (board: number[][]) => {
 
 const toggleArray = (
     board: number[][],
-    tilePos: { x: number; y: number },
+    tilePos: TilePos,
     nextTileIndex: -1 | 0 | 1
-) => {
+): number[][] => {
     const newBoard = _.cloneDeep(board);
     const { x, y } = tilePos;
 
@@ -92,7 +91,7 @@ const toggleArray = (
     return newBoard;
 };
 
-const reverseCoords = (pos: { x: number; y: number }) => ({
+const reverseCoords = (pos: TilePos): TilePos => ({
     y: pos.x,
     x: pos.y
 });
@@ -101,12 +100,12 @@ const reverseCoords = (pos: { x: number; y: number }) => ({
 const toggle = {
     hotizontal: (
         board: number[][],
-        tilePos: { x: number; y: number },
+        tilePos: TilePos,
         nextTileIndex: 1 | 0 | -1
     ) => toggleArray(board, tilePos, nextTileIndex),
     vertical: (
         board: number[][],
-        tilePos: { x: number; y: number },
+        tilePos: TilePos,
         nextTileIndex: 1 | 0 | -1
     ) =>
         matrixArray(
@@ -116,8 +115,8 @@ const toggle = {
 
 export const swipeTiles = (
     board: number[][],
-    tilePos: { x: number; y: number },
-    dir: { x: 1 | -1 | 0; y: 1 | -1 | 0 }
+    pos: TilePos,
+    dir: NextIndexes
 ): number[][] => {
 
     const isHorizontal = dir.x !== 0
@@ -126,7 +125,39 @@ export const swipeTiles = (
 
     return toggle[isHorizontal ? "hotizontal" : "vertical"](
         board,
-        tilePos,
+        pos,
         nextTileIndex
     );
 }
+
+// 
+
+export const calculateDirection = (
+    offsetX: number,
+    offsetY: number
+): NextIndexes => {
+    const isHorizontal = Math.abs(offsetX) > Math.abs(offsetY);
+
+    const x = offsetX > 0 ? 1 : -1;
+    const y = offsetY > 0 ? 1 : -1;
+
+    return {
+        x: 0 + (isHorizontal ? x : 0),
+        y: 0 + (!isHorizontal ? y : 0),
+    } as NextIndexes
+};
+
+export const calculateNewPosition = (
+    tilePos: TilePos,
+    direction: TilePos
+): TilePos => {
+    const x = tilePos.x + direction.x;
+    const y = tilePos.y + direction.y;
+
+    return { x, y };
+};
+
+export const isValidMove = (newPos: TilePos, boardSize: number): boolean => {
+    const coords = Object.values(newPos);
+    return coords.every(coord => coord >= 0 && coord <= boardSize - 1);
+};
