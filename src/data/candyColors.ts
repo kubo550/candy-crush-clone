b4f48c46138aab5b59be41b684413ad415997793
powerty <qwercy142@gmail.com>
2021-05-26 16:07:07 +0200

    create candys color data

---
+++ src/data/candyColors.ts
@@ -0,0 +1,34 @@
+type CandyData = {
+    primaryColor: string,
+    secondaryColor: string,
+}
+
+const candyData: CandyData[] = [
+    {
+        primaryColor: "#FE0000",
+        secondaryColor: "#3AC7CF",
+    },
+    {
+        primaryColor: "#FFEB00",
+        secondaryColor: "#3AC7CF",
+    },
+    {
+        primaryColor: "#00A8FF",
+        secondaryColor: "#3AC7CF",
+    },
+    {
+        primaryColor: "#AE00FF",
+        secondaryColor: "#3AC7CF",
+    },
+    {
+        primaryColor: "#FE6A00",
+        secondaryColor: "#3AC7CF",
+    },
+    {
+        primaryColor: "#00A800",
+        secondaryColor: "#3AC7CF",
+    },
+
+]
+
+export default candyData
