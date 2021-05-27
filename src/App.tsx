db598a0bda42888fbbd5a82e7d08abcc71f8e067
powerty <qwercy142@gmail.com>
2021-05-28 01:04:51 +0200

    Add newGame fn

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import _ from "lodash";
 import Board from "./components/Board/Board";
 import GameInfoBoard from "./components/GameInfoBoard/GameInfoBoard";
-import candyColorsData, { CandyData } from "./data/candyColors";
+import candyData, { CandyData } from "./data/candyColors";
+import { createBoard } from "./components/Board/Board.utils";
+import GameOver from "./components/GameOver/GameOver";
+
+const BOARD_SIZE = 10;
+const NUMBERS = [1, 2, 3, 4, 5];
+const MAX_MOVES = 15;
+const NUM_TILES_TO_GO = 5;
 
 const App = () => {
-  const [moves, setMoves] = useState(15);
-  const [tilesToGo, setTilesToGo] = useState(50);
+  const initalBoard = createBoard(BOARD_SIZE, NUMBERS);
+  const [board, setBoard] = useState<number[][]>(initalBoard);
+  const [isPlayable, setIsPLayable] = useState(true);
+  const [moves, setMoves] = useState(MAX_MOVES);
+  const [tilesToGo, setTilesToGo] = useState(NUM_TILES_TO_GO);
   const [tileToSearch, setTileToSearch] = useState<CandyData | null>(null);
 
   useEffect(() => {
-    const tileData = _.sample(candyColorsData)!;
+    const randomIdx = _.random(1, 5);
+    const tileData = candyData[randomIdx];
     setTileToSearch(tileData);
   }, []);
 
-  const move = () => setMoves(prev => prev - 1);
+  const move = useCallback(() => setMoves(prev => prev - 1), []);
 
   const subsTilesToGo = (id: number, quan: number) => {
     if (id !== tileToSearch?.tileId) {
@@ -23,6 +34,19 @@ const App = () => {
 
     setTilesToGo(prev => Math.max(prev - quan, 0));
   };
+
+  const newGame = () => {
+    setMoves(15);
+    setTilesToGo(35);
+
+    const randomIdx = _.random(1, 5);
+    const tileData = candyData[randomIdx];
+    setTileToSearch(tileData);
+
+    setBoard(createBoard(BOARD_SIZE, NUMBERS));
+  };
+
+  const gameOver = isPlayable && (moves === 0 || tilesToGo === 0);
 
   return (
     <div>
@@ -36,17 +60,25 @@ const App = () => {
       >
         Candy Crush Clone
       </h1>
-
       <GameInfoBoard
         moves={moves}
         tilesToGo={tilesToGo}
         tileToSearch={tileToSearch}
       />
       {tileToSearch?.tileId ? (
-        <Board move={move} subsTilesToGo={subsTilesToGo} />
+        <Board
+          move={move}
+          subsTilesToGo={subsTilesToGo}
+          isPlayable={isPlayable}
+          setIsPlayable={setIsPLayable}
+          board={board}
+          setBoard={setBoard}
+          numbers={NUMBERS}
+        />
       ) : (
         "loading"
       )}
+      {gameOver && <GameOver isWin={tilesToGo === 0} newGame={newGame} />}
     </div>
   );
 };
