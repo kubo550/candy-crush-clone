911c69fb8e5febc71bd8e5dec45b6cfd97ea5403
powerty <qwercy142@gmail.com>
2021-05-29 00:16:06 +0200

    Create candys color pallete

--- src/data/candyColors.ts
+++ src/data/candyColors.ts
@@ -1,44 +1,77 @@
 export type CandyData = {
-    primaryColor: string,
-    secondaryColor: string,
-    tileId: number
+    tileId: number;
+    primaryColor: string;
+    primaryColorLight: string;
+    secondaryColor: string;
+    secondaryColorLight: string;
+    thirdColor: string,
+    thirdColorLight: string,
 }
 
 const candyData: CandyData[] = [
     {
         tileId: 0,
         primaryColor: "#FE0000",
-        secondaryColor: "#3AC7CF",
+        primaryColorLight: "#F83232",
+        secondaryColor: "#7FFE00",
+        secondaryColorLight: "#A3F94E",
+        thirdColor: "#00FEFE",
+        thirdColorLight: "#56FCFC",
+
     },
     {
         tileId: 1,
         primaryColor: "#FFEB00",
-        secondaryColor: "#3AC7CF",
+        primaryColorLight: "#FDF273",
+        secondaryColor: "#00FF55",
+        secondaryColorLight: "#5DF991",
+        thirdColor: "#2B00FF",
+        thirdColorLight: "#4E2AFF",
     },
     {
         tileId: 2,
         primaryColor: "#00A8FF",
-        secondaryColor: "#3AC7CF",
+        primaryColorLight: "#54B5E5",
+        secondaryColor: "#5900FF",
+        secondaryColorLight: "#FDEE41",
+        thirdColor: "#FF00A6",
+        thirdColorLight: "#FF53C3",
     },
     {
         tileId: 3,
         primaryColor: "#AE00FF",
-        secondaryColor: "#3AC7CF",
+        primaryColorLight: "#BF36FF",
+        secondaryColor: "#D0FF00",
+        secondaryColorLight: "#FEF77D",
+        thirdColor: "#FF9500",
+        thirdColorLight: "#FFA62A",
     },
     {
         tileId: 4,
         primaryColor: "#FE6A00",
-        secondaryColor: "#3AC7CF",
+        primaryColorLight: "#FF832A",
+        secondaryColor: "#00FEA9",
+        secondaryColorLight: "#50FDC3",
+        thirdColor: "#5500FE",
+        thirdColorLight: "#7F3FFF",
     },
     {
         tileId: 5,
-        primaryColor: "#00A800",
-        secondaryColor: "#3AC7CF",
+        primaryColor: "#15A715",
+        primaryColorLight: "#2ACE2A",
+        secondaryColor: "#AA00A1",
+        secondaryColorLight: "#EB40E2",
+        thirdColor: "#4F900D",
+        thirdColorLight: "#7CCE2A",
     },
     {
         tileId: 6,
         primaryColor: "#FE0000",
-        secondaryColor: "#3AC7CF",
+        primaryColorLight: "#F83232",
+        secondaryColor: "#7FFE00",
+        secondaryColorLight: "#A3F94E",
+        thirdColor: "#00FEFE",
+        thirdColorLight: "#56FCFC",
     },
 
 ]
